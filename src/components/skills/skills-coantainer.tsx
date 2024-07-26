d7c98b42fd2bab1b40928baba2bd1f64bec0094c
imadJHR <imadjohar4@gmail.com>
2024-07-26 13:11:20 +0100

    fix bug

--- src/components/skills/skills-coantainer.tsx
+++ src/components/skills/skills-coantainer.tsx
@@ -5,21 +5,20 @@ import SkillItem from "./skill-item";
 import { Skills } from "@/types/interface";
 
 interface SkillsCoantainerProps {
-  
   label: string;
 
   data: Skills[];
 }
 
-const SkillsCoantainer: FC<SkillsCoantainerProps> = ({ label,data }) => {
+const SkillsCoantainer: FC<SkillsCoantainerProps> = ({ label, data }) => {
   return (
     <article className="min-h-[25rem] border border-primary rounded-lg">
       <h2 className="text-primary text-2xl font-semibold py-4 px-6 border-b border-primary">
         {label}
       </h2>
       <div className="px-6 py-4 spave-y-6">
-        {data.map((item)=>(
-          <SkillItem  key={item._id} {...item} />
+        {data.map((item) => (
+          <SkillItem item={item} key={item._id} {...item} />
         ))}
       </div>
     </article>
